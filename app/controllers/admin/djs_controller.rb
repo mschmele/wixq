dcc265e8aa1f07b5983ccf6af3b4d378dd1da6c9
Kyle Pollich <kpollich1@gmail.com>
2015-11-30 11:15:00 -0500

    Finish initial functionality of DJ admin/CRUD.

--- app/controllers/admin/djs_controller.rb
+++ app/controllers/admin/djs_controller.rb
@@ -1,9 +1,44 @@
 class Admin::DjsController < ApplicationController
-	def show
-		@dj = Dj.first
+	def create
+		@dj = Dj.new(dj_params)
+		if @dj.save
+			flash[:success] = "DJ \"" + @dj.first_name + " " + @dj.last_name + "\" Created!"
+			redirect_to admin_dj_path
+		else
+			render 'new'
+		end
 	end
 
 	def new
 		@dj = Dj.new
+	end
+
+	def edit
+		@dj = Dj.find(params[:id])
+	end
+
+	def show
+		@djs = Dj.all
+	end
+
+	def update
+		@dj = Dj.find(params[:id])
+		if @dj.update_attributes(dj_params)
+			# It worked!
+		else
+			render 'update'
+		end
+	end
+
+	def destroy
+		@dj = Dj.find(params[:id])
+		@dj.destroy
+		flash[:success] = "DJ \"" + @dj.first_name + " " + @dj.last_name + "\" Deleted!"
+		redirect_to admin_dj_path
+	end
+
+	private
+	def dj_params
+		params.require(:dj).permit(:first_name, :last_name)
 	end
 end
