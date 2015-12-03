249d7bc499e2a9333278ff7c14cddcf8ab9d8141
Kyle Pollich <kpollich1@gmail.com>
2015-12-03 14:51:55 -0500

    Initial functionality for creating new shows.

--- app/controllers/admin/shows_controller.rb
+++ app/controllers/admin/shows_controller.rb
@@ -1,8 +1,16 @@
 class Admin::ShowsController < ApplicationController
 	def create
+		@show = Show.new(show_params)
+		if @show.save
+			flash[:success] = "Show " + @show.name + " created!"
+			redirect_to admin_shows_path
+		else
+			render 'new'
+		end
 	end
 
 	def new
+		@show = Show.new
 	end
 
 	def edit
@@ -20,6 +28,7 @@ class Admin::ShowsController < ApplicationController
 
 	private
 	def show_params
+		params.require(:show).permit(:name, :start_time, :end_time, :day_of_week)
 	end
 
 end
