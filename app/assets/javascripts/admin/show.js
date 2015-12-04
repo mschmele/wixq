7896d4838db16e00773214b5913d5fa12fcb10a7
Kyle Pollich <kpollich1@gmail.com>
2015-12-04 18:35:18 -0500

    Admin: Create show form validation now works

--- app/assets/javascripts/admin/show.js
+++ app/assets/javascripts/admin/show.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
-	$("#new-show-form").validate({
-		onsubmit: true,
+	$(".new-show-form").validate({
+		debug: true,
+		rules: {
+			"show[name]": {required:true},
+			"show[start_time]": {required:true},
+			"show[end_time]": {required:true},
+			"show[day_of_week]": {required:true}
+		}
 	});
 });
