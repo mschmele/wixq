87b0a61c6ca8e953686ca87a44795979cc0533d5
Mark Schmele <mark.schmele@tegna.com>
2016-02-09 12:28:20 -0500

    Header: change 'now playing' text at breakpoint

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,4 +5,8 @@ class Show < ActiveRecord::Base
   def timeslot
     return self.start_time.strftime("%I:%M %p") + " to " + self.end_time.strftime("%I:%M %p")
   end
+
+	def duration
+		return self.end_time - self.start_time
+	end
 end
